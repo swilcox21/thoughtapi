68cd2eda7e1e7a32f21e39bdc0ab6f164551c747
Sam Wilcox <70547937+swilcox21@users.noreply.github.com>
2022-09-30 15:47:46 -0400

    Update settings.py
    adding vercel app to cors allowed origins

--- core/settings.py
+++ core/settings.py
@@ -139,7 +139,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",    
-    "http://localhost:3000",    
+    "http://localhost:3000",
+    "https://thought-org.vercel.app",
 ]
 
 REST_FRAMEWORK = {
